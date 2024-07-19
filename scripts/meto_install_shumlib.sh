8fcaa2bbeb94a9bd03f06667f2a05d4db9e31333
Matt Shin <matthew.shin@metoffice.gov.uk>
2024-07-19 10:51:46 +0100

    Import @7373 2024.03.1

--- scripts/meto_install_shumlib.sh
+++ scripts/meto_install_shumlib.sh
@@ -27,8 +27,8 @@
 # USAGE:  (Note - must be run from the toplevel Shumlib directory!)
 #   scripts/meto_install_shumlib.sh [xc40|x86|ex1a]
 #
-# This script was used to install shumlib version 2023.10.1
-# and was intended for use with the UM at UM 13.4
+# This script was used to install shumlib version 2024.03.1
+# and was intended for use with the UM at UM 13.5
 #
 
 set -eu
